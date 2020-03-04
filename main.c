9315cbe2a29f5884c60dd35789c7209911eb9b2d
Tao Shaocheng <912981655@qq.com>
2020-03-04 20:54:45 +0800

    lexical finished

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+extern FILE* yyin;
+extern int yylex();
+int main(int argc, char **argv){
+    if (argc > 1){
+        if (!(yyin = fopen(argv[1], "r"))){
+            perror(argv[1]);
+            return 1;
+        }
+    }
+    while (yylex() != 0);
+    return 0;
+}
